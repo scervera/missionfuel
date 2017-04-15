cd8dc9590c9bc15ea8d97e062191fe1bdbc30a9a
Steve Cervera <cervera1@mac.com>
2017-04-15 18:22:00 -0400

    Initial commmit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require bootstrap.min
+//= require circle.progress
+//= require custom.isotop
+//= require enscroll-0.6.2.min
+//= require functions
+//= require gmap.script
+//= require isotope.pkgd.min
+//= require jquery.flexslider
+//= require jquery.nstSlider.min
+//= require lightcase
+//= require masonry
+//= require swiper.min
+//= require TimeCircles
+//= require modernizr-2.8.3.min
+//= require_tree .
