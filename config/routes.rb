6bbd56b9f52631254059e4d53fb371fa6298cef0
Steve Cervera <cervera1@mac.com>
2017-04-23 15:51:16 -0400

    Created Statement of Beliefs page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'pages/beliefs'
+  match "beliefs" => "pages#beliefs", via: :get
+
+  get 'pages/board'
+  match "board" => "pages#board", via: :get
+
+  get 'pages/vision'
+  match "vision" => "pages#vision", via: :get
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
