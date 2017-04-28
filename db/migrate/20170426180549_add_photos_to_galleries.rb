23e53b0c78737e3c5599ed44596885d82cfefcbd
Steve Cervera <cervera1@mac.com>
2017-04-28 06:14:30 -0400

    updated photos migration to include json instead of string type for MySQL

--- db/migrate/20170426180549_add_photos_to_galleries.rb
+++ db/migrate/20170426180549_add_photos_to_galleries.rb
@@ -1,5 +1,6 @@
 class AddPhotosToGalleries < ActiveRecord::Migration[5.0]
   def change
-    add_column :galleries, :photos, :string
+    # add_column :galleries, :photos, :string  (for SQLite databases)
+    add_column :galleries, :photos, :json
   end
 end
