f7884a1a22ead3f4b80cbcece896abef7905191f
Steve Cervera <cervera1@mac.com>
2017-05-01 08:12:48 -0400

    configured action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "missionfuel_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.smtp_settings = {
+  address:              'smtpout.secureserver.net',
+  domain:               'missionfuel.net',
+  port:                 25,
+  user_name: ENV["MISSIONFUEL_EMAIL_USERNAME"],
+  password: ENV["MISSIONFUEL_EMAIL_PASSWORD"],
+  authentication:       'login',
+  openssl_verify_mode:  'none',
+  enable_starttls_auto: false 
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
