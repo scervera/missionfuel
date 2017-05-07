06756a7a9bd0aeff7500ac294b571524bd6cfc21
Steve Cervera <cervera1@mac.com>
2017-05-07 17:01:27 -0400

    changed application.js file to remove bootstrap as this comes with bootstrap-sass and was causing problems with dropdown function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require bootstrap.min
 //= require functions
 //= require circle.progress
 //= require custom.isotop
