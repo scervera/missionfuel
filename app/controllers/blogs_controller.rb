b9a1df949a2eb47119d0290f7d269f9540cee04f
Steve Cervera <cervera1@mac.com>
2018-05-30 13:59:27 -0400

    fixed issue where access to blogs did not require authentication

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:show]
   # GET /blogs
   # GET /blogs.json
   def index
